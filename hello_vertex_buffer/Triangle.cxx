5b30d0ebe9e4a34568a137a5725950b9d5b626ea
Carlo Wood <carlo@alinoe.com>
2023-04-23 14:21:49 +0200

    WIP on hello_vertex_buffer

--- hello_vertex_buffer/Triangle.cxx
+++ hello_vertex_buffer/Triangle.cxx
@@ -1,13 +1,13 @@
 #include "sys.h"
 #include "Triangle.h"
 
-Eigen::Vector2f const Triangle::position[batch_size] = {
+Eigen::Vector2f const Triangle::s_position[number_of_vertices] = {
   { 0.0f, -0.5f },      // A
   { -0.5f, 0.5f },      // B
   { 0.5f, 0.5f },       // C
 };
 
-Eigen::Vector3f const Triangle::color[batch_size] = {
+Eigen::Vector3f const Triangle::s_color[number_of_vertices] = {
   { 1.0f, 0.0f, 0.0f },  // A
   { 0.0f, 1.0f, 0.0f },  // B
   { 0.0f, 0.0f, 1.0f },  // C
--- hello_vertex_buffer/Triangle.h
+++ hello_vertex_buffer/Triangle.h
@@ -2,7 +2,6 @@
 
 #include "VertexData.h"
 #include "vulkan/shader_builder/VertexShaderInputSet.h"
-#include <Eigen/Geometry>
 
 class Triangle final : public vulkan::shader_builder::VertexShaderInputSet<VertexData>
 {
@@ -14,33 +13,33 @@ class Triangle final : public vulkan::shader_builder::VertexShaderInputSet<Verte
   //              /     \
   //             B-------C
   //
-  static constexpr int batch_size = 3;
+  static constexpr int number_of_vertices = 3;
 
   // The positions of the triangle corners are stored as 2D vectors; the z and w value are set in the shader.
-  static Eigen::Vector2f const position[batch_size];
+  static Eigen::Vector2f const s_position[number_of_vertices];
   // Colors are stored as a triplet of three float values for Red, Green and Blue respectively.
-  static Eigen::Vector3f const color[batch_size];
+  static Eigen::Vector3f const s_color[number_of_vertices];
 
  private:
   // Returns the total number of VertexData objects in the vertex buffer.
   int chunk_count() const override
   {
-    return batch_size;
+    return number_of_vertices;
   }
 
   // Returns the number of VertexData objects that are initialized by a single call to create_entry.
   int next_batch() override
   {
-    return batch_size;
+    return number_of_vertices;
   }
 
-  // Initialize the next `batch_size` VertexData objects.
+  // Initialize the next `number_of_vertices` VertexData objects.
   void create_entry(VertexData* input_entry_ptr) override
   {
-    for (int vertex = 0; vertex < batch_size; ++vertex)
+    for (int vertex = 0; vertex < number_of_vertices; ++vertex)
     {
-      input_entry_ptr[vertex].m_position = position[vertex];
-      input_entry_ptr[vertex].m_color = color[vertex];
+      input_entry_ptr[vertex].m_position = s_position[vertex];
+      input_entry_ptr[vertex].m_color = s_color[vertex];
     }
   }
 };
